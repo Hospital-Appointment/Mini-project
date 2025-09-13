ed5952ea98dea3769aca3d28959a24715c92b526
Sruthi4599 <kovvurisaisruthi@gmail.com>
2025-09-13 22:42:31 +0530

    small change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,3 @@
-
-import React from 'react'
-const App = () => {
-  return (
-    <div>
-      
-    </div>
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
