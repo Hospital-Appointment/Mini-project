f358c938346d1dc71fc66d16ebb1db416b614551
Sruthi4599 <kovvurisaisruthi@gmail.com>
2025-09-18 21:00:05 +0530

    My local changes before merging sample1

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: { 
+    extend: {
+    colors:{
+      'primary':"#5f6FFF"
+    }
+  } },
+  plugins: [],
+}
